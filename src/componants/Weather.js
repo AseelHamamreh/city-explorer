d78c6d4fc87fc1d4f23bca352486af2bfcf62eed
Aseel Hamamreh <aseel.hamamreh@hotmail.com>
2021-05-07 02:30:04 +0300

    lab08 done

--- src/componants/Weather.js
+++ src/componants/Weather.js
@@ -4,28 +4,28 @@ import Table from 'react-bootstrap/Table';
 export class Weather extends Component {
   render() {
     return(
-    <div style={{textAlign:'center', paddingLeft:'230px'}}>
-      <Table striped bordered hover style={{fontSize:'20px', width:'900px'}}>
-      <thead>
-        <tr>
-          <th>Data</th>
-          <th>Weather Description</th>
-        </tr>
-        </thead>
-        <tbody>
-      {this.props.expressArr.map(d => {
-        return(
-          <tr>
-            <td> {d.date} </td>
-            <td> low of {d.lowT}, high of {d.highT} with {d.description}. </td>
-          </tr>
-        )
-      })
-    }
-      </tbody>
-    </Table>
-    </div>
-    )
+      <div style={{textAlign:'center', paddingLeft:'230px'}}>
+        <Table striped bordered hover style={{fontSize:'20px', width:'900px'}}>
+          <thead>
+            <tr>
+              <th>Data</th>
+              <th>Weather Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.expressArr.map(d => {
+              return(
+                <tr>
+                  <td> {d.date} </td>
+                  <td> low of {d.lowT}, high of {d.highT} with {d.description}. </td>
+                </tr>
+              );
+            })
+            }
+          </tbody>
+        </Table>
+      </div>
+    );
   }
 }
 export default Weather;
